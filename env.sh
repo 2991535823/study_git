fbeca726c1d2de8cf5cfd69868633c982aa6ee7a
zhanggm <2991535823@qq.com>
2024-08-15 10:44:25 +0800

    load env

---
+++ env.sh
@@ -0,0 +1,4 @@
+PATH=$PATH:/home/bera/software_pack/T113/tina5.0_v1.0/out/toolchain/gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabi/bin
+ARCH=arm
+CROSS_COMPILE=arm-linux-gnueabi-
+export PATH ARCH CROSS_COMPILE
